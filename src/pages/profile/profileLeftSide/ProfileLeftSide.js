b8fcb7d2382b633621329b336c6687ce6a97afd1
Ibrahim-Sikder <ibrahimsikder5033@gmail.com>
2024-01-21 16:17:02 +0600

    visa-booking-page-b2c-add

--- src/pages/profile/profileLeftSide/ProfileLeftSide.js
+++ src/pages/profile/profileLeftSide/ProfileLeftSide.js
@@ -1,72 +1,71 @@
-import React from "react";
-import style from "./profile.module.css";
-import { FaAddressCard, FaBookmark, FaLock, FaRegUser } from "react-icons/fa";
-import { IoLogOut,IoCardOutline  } from "react-icons/io5";
-import ActiveUser from "../ActiveLink/ActiveUser";
+import React from "react"
+import style from "./profile.module.css"
+import { FaAddressCard, FaBookmark, FaLock, FaRegUser } from "react-icons/fa"
+import { IoLogOut, IoCardOutline } from "react-icons/io5"
+import ActiveUser from "../ActiveLink/ActiveUser"
 const ProfileLeftSide = () => {
-  const [age, setAge] = React.useState("");
+  const [age, setAge] = React.useState("")
   const handleChange = (event) => {
-    setAge(event.target.value);
-  };
+    setAge(event.target.value)
+  }
 
   return (
     <section>
       <div className={style.userProfileLeftSide}>
-        <div className={style.userInfo}>        
-           <ul className={style.userInfoList}>
+        <div className={style.userInfo}>
+          <ul className={style.userInfoList}>
             <li>
-            <ActiveUser href="/profile">
-             <div className="flex items-center">
-             <FaRegUser className="mr-2"  size={20}  />
-              <span>My Account </span>
-             </div>
-            </ActiveUser>
-            </li>
-            <li>
-            <ActiveUser href="/profile/booking">
-             <div className="flex items-center">
-             <FaBookmark className="mr-2" size={20}  />
-              <span>My Booking </span>
-             </div>
-            </ActiveUser>
+              <ActiveUser href="/profile">
+                <div className="flex items-center">
+                  <FaRegUser className="mr-2" size={20} />
+                  <span>My Account </span>
+                </div>
+              </ActiveUser>
             </li>
             <li>
-            <ActiveUser href="/profile/password">
-             <div className="flex items-center">
-             <FaLock className="mr-2"  size={20} />
-              <span>Change Password </span>
-             </div>
-            </ActiveUser>
+              <ActiveUser href="/profile/booking">
+                <div className="flex items-center">
+                  <FaBookmark className="mr-2" size={20} />
+                  <span>My Booking </span>
+                </div>
+              </ActiveUser>
             </li>
             <li>
-            <ActiveUser href="/profile/savecard">
-             <div className="flex items-center">
-             <FaAddressCard className="mr-2"  size={20} />
-              <span>Save Cards</span>
-             </div>
-            </ActiveUser>
+              <ActiveUser href="/profile/password">
+                <div className="flex items-center">
+                  <FaLock className="mr-2" size={20} />
+                  <span>Change Password </span>
+                </div>
+              </ActiveUser>
             </li>
             <li>
-            <ActiveUser href="/profile/savecard">
-             <div className="flex items-center">
-             <IoCardOutline className="mr-2"  size={20}  />
-              <span>Manage Your Data </span>
-             </div>
-            </ActiveUser>
+              <ActiveUser href="/profile/savecard">
+                <div className="flex items-center">
+                  <FaAddressCard className="mr-2" size={20} />
+                  <span>Save Cards</span>
+                </div>
+              </ActiveUser>
             </li>
-            
-            <li >
-            <div className="flex items-center ml-[15px]">
-             <IoLogOut className="mr-2" size={20} />
-              <span>Manage Your Data </span>
-             </div>
+            <li>
+              <ActiveUser href="/profile/manage">
+                <div className="flex items-center">
+                  <IoCardOutline className="mr-2" size={20} />
+                  <span>Manage Your Data </span>
+                </div>
+              </ActiveUser>
             </li>
-           </ul>
+
+            <li>
+              <div className="flex items-center ml-[15px] cursor-pointer">
+                <IoLogOut className="mr-2" size={20} />
+                <span>Log Out </span>
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
     </section>
-    
-  );
-};
+  )
+}
 
-export default ProfileLeftSide;
+export default ProfileLeftSide
